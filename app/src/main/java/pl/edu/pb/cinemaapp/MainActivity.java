721546008dded4357dd320c1634d3101770c3bcd
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-01-24 15:55:23 +0100

    Fixed add activity layout

--- app/src/main/java/pl/edu/pb/cinemaapp/MainActivity.java
+++ app/src/main/java/pl/edu/pb/cinemaapp/MainActivity.java
@@ -2,13 +2,28 @@ package pl.edu.pb.cinemaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addMovieButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        addMovieButton = findViewById(R.id.button_add_movie);
+
+        addMovieButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,AddEditMovie.class);
+                startActivity(intent);
+            }
+        });
     }
 }
