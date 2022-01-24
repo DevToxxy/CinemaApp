108f97fe7e32985b5934513023bcc71f53e709c6
DevToxxy <63885882+DevToxxy@users.noreply.github.com>
2022-01-24 22:39:14 +0100

    Adding movies works

--- app/src/main/java/pl/edu/pb/cinemaapp/AddEditMovie.java
+++ app/src/main/java/pl/edu/pb/cinemaapp/AddEditMovie.java
@@ -6,32 +6,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import java.util.List;
 
 public class AddEditMovie extends AppCompatActivity {
 
     private Button confirmButton, cancelButton;
+    EditText editMovieTitle, editMovieLength, editMovieAgeRating;
+    List<Movie> movieList;
+    MovieStorage movieStorage = MovieStorage.getInstance();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_edit_movie);
 
+        movieList = movieStorage.getMovies();
+
         confirmButton = findViewById(R.id.button_confirm);
         cancelButton = findViewById(R.id.button_cancel);
 
-        confirmButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AddEditMovie.this,MainActivity.class);
-                startActivity(intent);
-            }
+        editMovieTitle = findViewById(R.id.edit_movie_title);
+        editMovieLength = findViewById(R.id.edit_movie_length);
+        editMovieAgeRating = findViewById(R.id.edit_age_rating);
+
+
+        confirmButton.setOnClickListener(v -> {
+
+            Movie newMovie = new Movie(editMovieTitle.getText().toString(),
+                    Integer.parseInt(editMovieLength.getText().toString()),
+                    Integer.parseInt(editMovieAgeRating.getText().toString()));
+            movieStorage.addMovie(newMovie);
+            Intent intent = new Intent(AddEditMovie.this,MainActivity.class);
+            startActivity(intent);
         });
 
-        cancelButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(AddEditMovie.this,MainActivity.class);
-                startActivity(intent);
-            }
+        cancelButton.setOnClickListener(v -> {
+            Intent intent = new Intent(AddEditMovie.this,MainActivity.class);
+            startActivity(intent);
         });
 
     }
